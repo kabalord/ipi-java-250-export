b56c32709ee4ced57c6d493272ad7ab8f8b185c7
walter roa serrano <walterroaserrano@Admins-MacBook-Pro.local>
2019-02-22 15:20:35 +0100

    crée un fichier excel, crée un onglet, crée une ligne et le parcourir

--- src/main/java/com/example/demo/controller/ExportController.java
+++ src/main/java/com/example/demo/controller/ExportController.java
@@ -2,6 +2,11 @@ package com.example.demo.controller;
 
 import com.example.demo.entity.Client;
 import com.example.demo.service.ClientService;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +17,8 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
@@ -32,7 +39,7 @@ public class ExportController {
         PrintWriter writer = response.getWriter();
         List<Client> allClients = clientService.findAllClients();
         writer.println("Id;Nom;Prenom;Date naissance;Age");
-        // for(Client cliente : allClients)
+        // for(Client client : allClients)
         LocalDate now = LocalDate.now();
         for (Iterator<Client> i = allClients.iterator(); i.hasNext();){
             Client client = i.next();
@@ -43,7 +50,56 @@ public class ExportController {
                     + client.getDateNaissance() + ";"
                     + (now.getYear() - client.getDateNaissance().getYear())
             );
+
+
         }
+    }
+
+    // pour obtenir une cell dans un fichier excel
+
+
+    @GetMapping("/clients/xlsx")
+    public void clientsXLSX(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/xlsx");
+        response.setHeader("Content-Disposition", "attachment; filename=\"clients.xlsx\"");
+
+        List<Client> allClients = clientService.findAllClients();
+        LocalDate now = LocalDate.now();
+
+        Workbook workbook = new XSSFWorkbook(); // crée un fichier excel
+        Sheet sheet = workbook.createSheet("Clients"); // crée un onglet
+        Row headerRow = sheet.createRow(0); // crée une ligne
+
+        ArrayList<String> celTitres = new ArrayList<String>() ;
+        celTitres.add("Matricule");
+        celTitres.add("Prénom");
+        celTitres.add("Nom");
+        celTitres.add("Date de Naissance");
+        celTitres.add("Age");
+
+        int i =0;
+        for(String title : celTitres){
+            headerRow.createCell(i).setCellValue(title);
+            i++;
+        }
+        //Cell cellPrenom = headerRow.createCell(0); // crée une cellule
+        //cellPrenom.setCellValue("Matricule;Prénom;Nom;Date de Naissance"); // ajoute une valeur dans la cellule
+
+        int rowNum = 1;
+        for(Client client : allClients){
+            Row row = sheet.createRow(rowNum++);
+            row.createCell(0).setCellValue(client.getId());
+            row.createCell(1).setCellValue(client.getPrenom());
+            row.createCell(2).setCellValue(client.getNom());
+            row.createCell(3).setCellValue(client.getDateNaissance().format(DateTimeFormatter.ofPattern("dd/MM/YYYY")));
+            row.createCell(4).setCellValue((now.getYear()-client.getDateNaissance().getYear()));
+        }
+
+
+
+        workbook.write(response.getOutputStream());
+        workbook.close();
+
     }
 
 }
